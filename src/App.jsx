6330a656559fcbdf640f40efc50e96a6a6ce6900
MayDay <61630498+MoomieMay@users.noreply.github.com>
2025-06-27 16:48:30 -0300

    uppercase home

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Prudencia from "./pages/Prudencia";
 import Foucault from "./pages/Foucault";
 import Mal from "./pages/Mal";
